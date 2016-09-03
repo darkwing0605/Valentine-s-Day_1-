226e317159ef8a994d0f47ab0e82084c6bc9a854
Hailong Tian <2553292615@qq.com>
2016-09-03 22:38:41 +0800

    Create dhteumeuleu.js

---
+++ dhteumeuleu.js
@@ -0,0 +1,35 @@
+// ================================
+// written by Dark Wing - December 12, 2014
+//
+// Love you forever
+// ================================
+
+id = function(o) {
+	return document.getElementById(o);
+}
+
+px = function (x) {
+	return ''.concat(Math.round(x), 'px');
+}
+
+pxLeft = function(o) {
+	for (var x = 0; o != null; o = o.offsetParent) x += o.offsetLeft;
+	return x;
+}
+
+pxTop = function(o) {
+	for (var x = 0; o != null; o = o.offsetParent) x += o.offsetTop;
+	return x;
+}
+
+/* ==== DOM 2 add event ==== */
+addEvent = function (o, e, f) {
+	var r = false;
+	if (window.addEventListener) {
+		o.addEventListener(e, f, false);
+		r = true;
+	} else if (window.attachEvent) {
+		r = o.attachEvent('on' + e, f);
+	}
+	return r;
+}
